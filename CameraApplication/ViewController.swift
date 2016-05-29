57101742640cd2c3b0813f9609a903021ea46c7b
ftwdjw <ftwdjw@gmail.com>
2016-05-29 13:38:01 -0600

    start of camera application using Swift for the iPhone.

--- CameraApplication/ViewController.swift
+++ CameraApplication/ViewController.swift
@@ -8,18 +8,58 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {//begin
+    
+    @IBOutlet var imageView: UIImageView!
+    
+    
 
-    override func viewDidLoad() {
+    override func viewDidLoad() {//begin
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+        
+        
+        
+        
+    }//end
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning() {//begin
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }//end
+    
+    @IBAction func chooseImageFromPhotoLibrary() {
+        let picker = UIImagePickerController()
+        
+        picker.delegate = self
+        picker.sourceType = .PhotoLibrary
+        presentViewController(picker, animated: true, completion: nil)
+        
+        print("choose existing photo")
     }
+    
+    
+    @IBAction func chooseFromCamera(){
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .Camera
+        
+        presentViewController(picker, animated: true, completion: nil)
+        
+        print("take photo with camera")
 
+        
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        
+        imageView.image = info[UIImagePickerControllerOriginalImage] as? UIImage
+        dismissViewControllerAnimated(true, completion: nil)
+     
+    }
+    
+    
 
-}
+}//end
 
